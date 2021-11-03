50da28f60a260a41ab5af80ae27ebfe9f39e4343
henryque <henryque_1@hotmail.com>
2021-11-03 17:08:06 -0300

    feat(index.js): add customer create route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
+
+const customers = [];
 
 const app = express();
 
 app.use(express.json());
 
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuid();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: []
+  });
+
+  return res.status(201).send('Customer was successfully created!');
+
+});
 
 
 app.listen(3333, () => console.log("Server is running!"));
